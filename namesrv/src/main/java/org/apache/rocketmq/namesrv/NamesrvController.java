6a14512b97da4beee07103b53042e908ad2f8662
pirate <hubaofei@sdbattery.com>
2021-02-20 14:31:57 +0800

    add log

--- namesrv/src/main/java/org/apache/rocketmq/namesrv/NamesrvController.java
+++ namesrv/src/main/java/org/apache/rocketmq/namesrv/NamesrvController.java
@@ -41,20 +41,39 @@ import org.apache.rocketmq.srvutil.FileWatchService;
 
 public class NamesrvController {
     private static final InternalLogger log = InternalLoggerFactory.getLogger(LoggerName.NAMESRV_LOGGER_NAME);
-
+    /**
+     * NameServer配置属性：包括rocketmqHome（RocketMQ home目录），kvConfigPath（KV配置文件路径），configStorePath（Store配置文件路径）等
+     */
     private final NamesrvConfig namesrvConfig;
 
+    /**
+     * Netty的相关配置数据行
+     */
     private final NettyServerConfig nettyServerConfig;
 
+    /**
+     *  NamesrvController 定时任务执行线程池，包含两个任务
+     */
     private final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor(new ThreadFactoryImpl(
         "NSScheduledThread"));
+    /**
+     * KV配置属性管理器，主要管理NameServer的配置
+     */
     private final KVConfigManager kvConfigManager;
+    /**
+     * NameServer数据的载体，记录Broker,Topic等信息
+     */
     private final RouteInfoManager routeInfoManager;
-
+    /**
+     * Netty Server
+     */
     private RemotingServer remotingServer;
 
     private BrokerHousekeepingService brokerHousekeepingService;
 
+    /**
+     * 执行Netty Server的线程池
+     */
     private ExecutorService remotingExecutor;
 
     private Configuration configuration;
@@ -74,16 +93,17 @@ public class NamesrvController {
     }
 
     public boolean initialize() {
-
+        // 1. 从/${user.home}/namesrv/kvConfig.json 中加载NameServer的配置
         this.kvConfigManager.load();
-
+        // 2. 创建Netty Server
         this.remotingServer = new NettyRemotingServer(this.nettyServerConfig, this.brokerHousekeepingService);
-
+        // 3. 创建Netty Server执行的线程池
+        // 该参数目前主要用于NameServer的默认业务线程池，处理诸如broker,product,consume与NameServer的所有交互命令
         this.remotingExecutor =
             Executors.newFixedThreadPool(nettyServerConfig.getServerWorkerThreads(), new ThreadFactoryImpl("RemotingExecutorThread_"));
-
+        // 4. 注册NameServer服务接受请求的处理类
         this.registerProcessor();
-
+        // 5. 每隔10s扫描broker，维护当前存活的Broker信息
         this.scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
 
             @Override
@@ -92,6 +112,7 @@ public class NamesrvController {
             }
         }, 5, 10, TimeUnit.SECONDS);
 
+        // 6. 每隔10s打印KVConfig信息
         this.scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
 
             @Override
